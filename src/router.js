dbb906922f508802afb52d1936d6b47b7036431f
liaoyuezhen <1315472685@qq.com>
2020-03-17 16:31:15 +0800

    提交

--- src/router.js
+++ src/router.js
@@ -5,18 +5,24 @@ import detail from './views/newDetail/detail'
 import giftDetail from './views/newDetail/giftDetail'
 import test from './views/test'
 import list from './views/list'
+import getterSetter from './views/getterSetter'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/',
+      name: 'getterSetter',
+      component: getterSetter
+    },
+    {
       path: '/test',
       name: 'test',
       component: test
     },
     {
-      path: '/',
+      path: '/list',
       name: 'list',
       component: list
     },
---
+++ src/views/getterSetter/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="setter">
+    <p></p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'getterSetter',
+  data () {
+    return {
+      obj: null
+    }
+  },
+  created () {
+    this.obj = { age: 12 }
+    this.$set(this.obj, 'sex', 'grils')
+    let obj = { name: 'haha' }
+    this.obj = { ...this.obj, ...obj }
+    console.log(this.obj)
+  }
+}
+</script>
